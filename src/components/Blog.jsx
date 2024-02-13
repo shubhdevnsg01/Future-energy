99295ef073b9b0147f3ee169002413703461c014
shubhdevnsg01 <petrifiedmaniac@gmail.com>
2024-02-13 12:17:46 +0530

    Travel page completed full, working on blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,28 @@
 const Blog=()=>{
     return(
         <>
-        <div>Blog section</div>
+         <div className="app_Blog">
+      <div className="top-content_Blog">
+        <h1>Title Here</h1>
+        <p>Content goes here</p>
+      </div>
+      <div className="card-container_Blog">
+        <div className="card_Blog">
+          <img src="image1.jpg" alt="Card 1" />
+          <div className="card-content_Blog">
+            <h2>Card 1 Title</h2>
+            <p>Card 1 content goes here</p>
+          </div>
+        </div>
+        <div className="card_Blog">
+          <img src="image2.jpg" alt="Card 2" />
+          <div className="card-content_Blog">
+            <h2>Card 2 Title</h2>
+            <p>Card 2 content goes here</p>
+          </div>
+        </div>
+      </div>
+    </div>
         </>
     )
 }
--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -3,11 +3,15 @@ import TravelIcon1 from './../assets/icon/travel-icon.png'
 import TravelIcon2 from './../assets/icon/travel-icon2.png'
 import TravelIcon3 from './../assets/icon/travel-icon3.png'
 import TravelIcon4 from './../assets/icon/travel-icon4.png'
-import facebook from './../assets/icon/facebook.png'
+import London from './../assets/images/London.jpg'
+import location1 from './../assets/images/1.jpg'
+import location2 from './../assets/images/2.jpg'
+import location3 from './../assets/images/3.jpg'
 
 const Travel =()=>{
     return(
         <>
+        <div>
          <div className="select-offers-for-traveling">
       <div className="title">
         
@@ -43,6 +47,71 @@ const Travel =()=>{
         </div>
       </div>
     </div>
+    </div>
+    <div>
+    <div className="container_2">
+      <div className="title_2">
+        <h1>Welcome to My Website</h1>
+      </div>
+      <div className="image-container_2">
+        <img src={London} alt="Background" />
+      </div>
+    </div>
+    </div>
+    <div>
+    <div class="container_3">
+  <div class="header_3">
+    <h1>Best Places Around</h1>
+    <p>There are many variations of passages of Lorem  Ipsum available</p>
+  </div>
+  <div class="cards_3">
+    <div class="card_3">
+      <img src={location1} alt="Image 1"/>
+      <div class="card-text_3">
+        <h2>Holiday Tour</h2>
+        <p>There are many variations of passages</p>
+        <p>Lorem Ipsum available,but the majority have</p>
+        <p>suffered alteration in soe suffk even slightly</p>
+        <p> believable. If y be sure theres</p>
+      </div>
+    </div>
+    <div class="card_3">
+      <img src={location2} alt="Image 2"/>
+      <div class="card-text_3">
+      <h2>New York</h2>
+      <p>There are many variations of passages</p>
+        <p>Lorem Ipsum available,but the majority have</p>
+        <p>suffered alteration in soe suffk even slightly</p>
+        <p> believable. If y be sure theres</p>
+      </div>
+    </div>
+    <div class="card_3">
+      <img src={location3} alt="Image 3"/>
+      <div class="card-text_3">
+      <h2>London</h2>
+      <p>There are many variations of passages</p>
+        <p>Lorem Ipsum available,but the majority have</p>
+        <p>suffered alteration in soe suffk even slightly</p>
+        <p> believable. If y be sure theres</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    <div>
+    <div class="container_4">
+  <div class="content_4">
+    <h1>ITALY TRAVEL GUIDE</h1>
+    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by</p>
+    <p>Injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,</p>
+    <p>you need to be sure there</p>
+    <div class="buttons_4">
+      <button>Book Now</button>
+      <button>Get More</button>
+      </div>
+      </div>
+      </div>
+      </div>
         </>
     )
 }
